c958118d4990cbe48ed00e1bbca104396beba0c6
SujendraNayak <sujinayak008@gmail.com>
2024-09-29 11:48:19 +0530

    Card Section

--- frontend/src/Components/CardSection.js
+++ frontend/src/Components/CardSection.js
@@ -8,8 +8,8 @@ import img4 from "../assets/images/img4.jpg";
 import img5 from "../assets/images/img5.jpg";
 import img6 from "../assets/images/img6.jpg";
 import img7 from "../assets/images/img7.jpg";
-//import img8 from "../assets/images/img8.jpg";
-//import img9 from "../assets/images/img9.jpg";
+import img8 from "../assets/images/img8.jpg";
+import img9 from "../assets/images/img9.jpg";
 //import img11 from "../assets/images/img11.jpg";
 //import img12 from "../assets/images/img12.jpg";
 
